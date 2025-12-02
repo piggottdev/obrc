0c688cfbea15cd7ed6bc01e2ec8f784c047daef2
PiggottDev <piggottdev@gmail.com>
2025-12-02 01:28:01 +0000

    Multi-threaded

--- src/main/java/dev/pig/obrc/CalculateAverage.java
+++ src/main/java/dev/pig/obrc/CalculateAverage.java
@@ -1,10 +1,14 @@
 package dev.pig.obrc;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
 
@@ -19,55 +23,117 @@ public class CalculateAverage {
         System.out.println(run(input));
     }
 
-    private static class MeasurementAggregator {
-        private double min = Double.POSITIVE_INFINITY;
-        private double max = Double.NEGATIVE_INFINITY;
-        private double sum;
-        private long count;
-    }
-
-    private record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
-    }
-
-    private record ResultRow(double min, double mean, double max) {
-
-        public String toString() {
-            return round(min) + "/" + round(mean) + "/" + round(max);
-        }
-
-        private double round(double value) {
-            return Math.round(value * 10.0) / 10.0;
-        }
-    }
-
     public static String run(final String input) throws IOException {
-        final Collector<Measurement, MeasurementAggregator, ResultRow> collector = Collector.of(
-                MeasurementAggregator::new,
-                (a, m) -> {
-                    a.min = Math.min(a.min, m.value);
-                    a.max = Math.max(a.max, m.value);
-                    a.sum += m.value;
-                    a.count++;
-                },
-                (agg1, agg2) -> {
-                    var res = new MeasurementAggregator();
-                    res.min = Math.min(agg1.min, agg2.min);
-                    res.max = Math.max(agg1.max, agg2.max);
-                    res.sum = agg1.sum + agg2.sum;
-                    res.count = agg1.count + agg2.count;
 
-                    return res;
-                },
-                agg -> new ResultRow(agg.min, (Math.round(agg.sum * 10.0) / 10.0) / agg.count, agg.max));
+        final Map<String, Entry> result = chunkify(input).parallelStream()
+                .flatMap(chunk -> processChunk(chunk).entrySet().stream())
+                .collect(Collectors.<Map.Entry<String, Entry>, String, Entry, TreeMap<String, Entry>>toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        Entry::merge,
+                        TreeMap::new
+                ));
 
-        final Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(input))
-                .map(l -> new Measurement(l.split(";")))
-                .collect(groupingBy(Measurement::station, collector)));
+        return result.toString();
+    }
 
-        return measurements.toString();
+    private static List<MappedByteBuffer> chunkify(final String filename) throws IOException {
+        final RandomAccessFile file = new RandomAccessFile(filename, "r");
+
+        final long processorCount = Runtime.getRuntime().availableProcessors();
+        final long fileSize = file.length();
+        final long chunkSize = Math.min(Integer.MAX_VALUE - 1000, fileSize / processorCount);
+        final int chunkCount =  (int) (fileSize / chunkSize) + 1;
+
+        final List<MappedByteBuffer> chunks = new ArrayList<>(chunkCount);
+
+        long head = 0L;
+
+        // For each chunk
+        while (head < fileSize) {
+
+            // Jump to the back
+            long tail = head + chunkSize;
+            // Find either the next new line character or the EOF
+            if (tail > fileSize) {
+                tail = fileSize;
+            } else {
+                file.seek(tail);
+                while (file.read() != '\n') {
+                    tail++;
+                }
+                tail++;
+            }
+
+            // Add the chunk to the list and set the head to the tail
+            chunks.add(file.getChannel().map(FileChannel.MapMode.READ_ONLY, head, tail - head));
+            head = tail;
+        }
+
+        return chunks;
+    }
+
+    private static Map<String, Entry> processChunk(final MappedByteBuffer chunk) {
+
+        final Map<String, Entry> stations = new HashMap<>();
+
+        while (chunk.position() < chunk.capacity()) {
+
+            // Mark the start of the line
+            final int nameStart = chunk.position();
+            // Move the position to one after the semicolon
+            while (chunk.get() != ';') {}
+            // Grab the name into a byte slice
+            final byte[] name =  new byte[(chunk.position()-1) - nameStart];
+            chunk.get(nameStart, name);
+
+            // Mark the start of the temperature reading
+            final int tempStart = chunk.position();
+            // Move the position to one after the newline
+            while (chunk.get() != '\n') {}
+            // Grab the temperature into a byte slice
+            final byte[] temp =  new byte[(chunk.position()-1) - tempStart];
+            chunk.get(tempStart, temp);
+
+            stations.computeIfAbsent(new String(name), k -> new Entry()).add(Double.parseDouble(new String(temp)));
+        }
+
+        return stations;
+    }
+
+    private static class Entry {
+
+        private long count = 0;
+        private double sum = 0;
+        private double max = Double.NEGATIVE_INFINITY;
+        private double min = Double.POSITIVE_INFINITY;
+
+        private Entry add(final double val) {
+            this.count++;
+            this.sum += val;
+            this.max = Math.max(this.max, val);
+            this.min = Math.min(this.min, val);
+            return this;
+        }
+
+        private Entry merge(final Entry other) {
+            this.count += other.count;
+            this.sum += other.sum;
+            this.max = Math.max(max, other.max);
+            this.min = Math.min(min, other.min);
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return round(min) + "/" + round(sum / count) + "/" + round(max);
+        }
+
+        private static double round(final double val) {
+            return Math.round(val * 10.0) / 10.0;
+        }
+
     }
 
 }
+
